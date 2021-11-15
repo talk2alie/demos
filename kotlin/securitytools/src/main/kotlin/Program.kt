7469c5093948090421ae3a788c979bff9aa9836b
Mohamed Alie Pussah, II <talk2alie@outlook.com>
2021-11-14 21:02:04 -0500

    Add using higher order functions in Kotlin

--- kotlin/securitytools/src/main/kotlin/Program.kt
+++ kotlin/securitytools/src/main/kotlin/Program.kt
@@ -1,12 +1,21 @@
 fun main(args: Array<String>) {
-    /*val providers = getProviders();
+    displayAllProviders{
+            key, value -> println("\t$key: $value")
+    }
+}
+
+fun displayAllProviders(displayFunction: (String, String) -> Unit): Unit {
+    val providers = getProviders();
     var count = 0;
     providers.forEach { provider ->
         println();
         println("${++count}: ${provider.name}");
         provider.forEach { key, value ->
-            println("\t$key: $value")
+            displayFunction(key.toString(), value.toString())
         }
-    }*/
+    }
+}
 
+fun display(key: String, value: String): Unit {
+    println("\t$key: $value")
 }
